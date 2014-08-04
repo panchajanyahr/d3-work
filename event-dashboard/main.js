6338ac41d2e9abfb6e0e9ed9ade1d09273b1819f
Panchajanya HR <panchajanyahr@gmail.com>
2014-08-04 06:17:33 +0000

    Added 2 more charts

--- event-dashboard/main.js
+++ event-dashboard/main.js
@@ -1,13 +1,29 @@
 d3.csv("bi32.csv", function(error, data) {
-    var container = d3.select(".total-revenue");
-    var nested_data = d3.nest()
+    var totalRevenueData = d3.nest()
         .key(function(d) { return d["eventname"].trim(); })
         .rollup(function(events) { return d3.sum(events, function(d) {
             return parseInt(d["  value  "].replace("$","").replace(",", ""));
         });})
         .entries(data);
-    console.log(nested_data);
-    var options = {yHeading:"Revenue",
-                   data:nested_data};
-    barChart(container, options);
+
+    var averageRevenueData = d3.nest()
+        .key(function(d) { return d["eventname"].trim(); })
+        .rollup(function(events) {
+            var totalRevenue = d3.sum(events, function(d) { return parseInt(d["  value  "].replace("$","").replace(",", "")); });
+            return totalRevenue/events.length;
+        })
+        .entries(data);
+
+    var perHeadRevenueData = d3.nest()
+        .key(function(d) { return d["eventname"].trim(); })
+        .rollup(function(events) {
+            var totalRevenue = d3.sum(events, function(d) { return parseInt(d["  value  "].replace("$","").replace(",", "")); });
+            var totalCapacity = d3.sum(events, function(d) { return parseInt(d["capacity"]); });
+            return totalRevenue/totalCapacity;
+        })
+        .entries(data);
+
+    barChart(d3.select(".total-revenue"), {yHeading:"Revenue", data:totalRevenueData});
+    barChart(d3.select(".average-revenue"), {yHeading:"Revenue by suite", data:averageRevenueData});
+    barChart(d3.select(".per-head-revenue"), {yHeading:"Average", data:perHeadRevenueData});
 });

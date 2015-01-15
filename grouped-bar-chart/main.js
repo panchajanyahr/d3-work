4fd5c52cdf33b883f15c5edd462588aec4f7b80c
Panchajanya HR <panchajanyahr@gmail.com>
2015-01-15 22:01:03 +0530

    Showing Y Axis

--- grouped-bar-chart/main.js
+++ grouped-bar-chart/main.js
@@ -1,5 +1,12 @@
-var width = 960;
-var height = 500;
+var fullWidth = 960;
+var fullHeight = 500;
+var margin = {
+    left: 50,
+    right: 0,
+    bottom: 50,
+    top:0
+};
+
 var padding = {
     bar: 5,
     sample: 50,
@@ -7,9 +14,12 @@ var padding = {
 };
 
 d3.json("data.json", function(error, chartData) {
+    var width = fullWidth - margin.left - margin.right;
+    var height = fullHeight - margin.top - margin.bottom;
+
     var chart = d3.select(".chart")
-        .attr("width", width)
-        .attr("height", height);
+        .attr("width", fullWidth)
+        .attr("height", fullHeight);
 
     var queryIndex = -1;
     var sampleIndex = -1;
@@ -37,9 +47,22 @@ d3.json("data.json", function(error, chartData) {
         })
     );
 
+    var chartArea = chart
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
     var y = d3.scale.linear()
         .range([height, 0])
         .domain([0, d3.max(flatData, function(d) { return d.value; })]);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    chart.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .call(yAxis);
 
     var queryCount = _.keys(chartData).length;
 
@@ -49,7 +72,7 @@ d3.json("data.json", function(error, chartData) {
                     (queryCount - 1) * padding.query
                    ) / flatData.length;
 
-    var bar = chart.selectAll("g")
+    var bar = chartArea.selectAll("g")
         .data(flatData)
         .enter().append("g")
         .attr("transform", function(d, i) {
@@ -68,8 +91,12 @@ d3.json("data.json", function(error, chartData) {
 
     bar.append("text")
         .attr("x", barWidth / 2)
-        .attr("y", function(d) { return y(d.value) + 3; })
+        .attr("y", function(d) {
+            return y(d.value) + 3;
+        })
         .attr("dy", ".75em")
         .attr("fill", "white")
-        .text(function(d) { return d.value; });
+        .text(function(d) {
+            return d.key;
+        });
 });

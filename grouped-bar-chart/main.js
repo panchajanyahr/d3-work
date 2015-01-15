47f78be63fbf25522e0d927fa84f667bbffcc35c
Panchajanya HR <panchajanyahr@gmail.com>
2015-01-15 21:08:08 +0530

    Adding padding between queries

--- grouped-bar-chart/main.js
+++ grouped-bar-chart/main.js
@@ -6,34 +6,49 @@ d3.json("data.json", function(error, data) {
         .attr("width", width)
         .attr("height", height);
 
+    var queryIndex = -1;
     var flatData = _.flatten(
         _.map(data, function(samples, queryName) {
+            queryIndex++;
+            var sampleIndex = -1;
+
             return _.map(samples, function(values, sampleName) {
+                sampleIndex++;
                 return _.map(values, function(value, key) {
-                    var result = {};
-                    result.key = key;
-                    result.value = value;
-                    result.sample = sampleName;
-                    result.query = queryName;
-                    return result;
+                    return {
+                        key: key,
+                        value: value,
+                        sample: sampleName,
+                        query: queryName,
+                        queryIndex: queryIndex,
+                        sampleIndex: sampleIndex
+                    };
                 });
             });
         })
     );
 
     var barPadding = 5;
+    var queryPadding = 50;
 
     var y = d3.scale.linear()
         .range([height, 0])
         .domain([0, d3.max(flatData, function(d) { return d.value; })]);
 
-    var barWidth = (width - (flatData.length + 1) * barPadding) / flatData.length;
+    var queryCount = _.keys(data).length;
+
+    var barWidth = (width -
+                    (flatData.length + 1) * barPadding -
+                    (queryCount - 1) * queryPadding
+                   ) / flatData.length;
 
     var bar = chart.selectAll("g")
         .data(flatData)
         .enter().append("g")
         .attr("transform", function(d, i) {
-            var x = (i * barWidth) + ((i + 1) * barPadding);
+            var x = (i * barWidth) +
+                d.queryIndex * queryPadding +
+                ((i + 1) * barPadding);
             return "translate(" + x + ",0)";
         });
 

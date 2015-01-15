dc7c20fe7c6805443d3c0582787040e4c9515f6b
Panchajanya HR <panchajanyahr@gmail.com>
2015-01-15 21:28:32 +0530

    Grouped padding

--- grouped-bar-chart/main.js
+++ grouped-bar-chart/main.js
@@ -1,7 +1,12 @@
-d3.json("data.json", function(error, data) {
-    var width = 960;
-    var height = 500;
+var width = 960;
+var height = 500;
+var padding = {
+    bar: 5,
+    sample: 50,
+    query: 100
+};
 
+d3.json("data.json", function(error, chartData) {
     var chart = d3.select(".chart")
         .attr("width", width)
         .attr("height", height);
@@ -12,7 +17,7 @@ d3.json("data.json", function(error, data) {
     var sampleCount = 0;
 
     var flatData = _.flatten(
-        _.map(data, function(samples, queryName) {
+        _.map(chartData, function(samples, queryName) {
             queryIndex++;
             sampleCount += _.keys(samples).length;
 
@@ -32,35 +37,32 @@ d3.json("data.json", function(error, data) {
         })
     );
 
-    var barPadding = 5;
-    var samplePadding = 20;
-    var queryPadding = 50;
-
     var y = d3.scale.linear()
         .range([height, 0])
         .domain([0, d3.max(flatData, function(d) { return d.value; })]);
 
-    var queryCount = _.keys(data).length;
+    var queryCount = _.keys(chartData).length;
 
     var barWidth = (width -
-                    (flatData.length + 1) * barPadding -
-                    (sampleCount - 1) * samplePadding -
-                    (queryCount - 1) * queryPadding
+                    (flatData.length + 1) * padding.bar -
+                    (sampleCount - 1) * padding.sample -
+                    (queryCount - 1) * padding.query
                    ) / flatData.length;
 
     var bar = chart.selectAll("g")
         .data(flatData)
         .enter().append("g")
         .attr("transform", function(d, i) {
-            var padding = (d.queryIndex * queryPadding +
-                           d.sampleIndex * samplePadding +
-                           ((i + 1) * barPadding));
-            var x = (i * barWidth) + padding;
+            var xPadding = (d.queryIndex * padding.query +
+                            d.sampleIndex * padding.sample +
+                            ((i + 1) * padding.bar));
+            var x = (i * barWidth) + xPadding;
             return "translate(" + x + ",0)";
         });
 
     bar.append("rect")
         .attr("y", function(d) { return y(d.value); })
+        .attr("fill", "blue")
         .attr("height", function(d) { return height - y(d.value); })
         .attr("width", barWidth);
 
@@ -68,5 +70,6 @@ d3.json("data.json", function(error, data) {
         .attr("x", barWidth / 2)
         .attr("y", function(d) { return y(d.value) + 3; })
         .attr("dy", ".75em")
+        .attr("fill", "white")
         .text(function(d) { return d.value; });
 });
